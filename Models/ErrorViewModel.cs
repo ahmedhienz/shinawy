3b82a03979d45ec2894ea63fbdd53cf26669f3a5
ahmedhienz <44682086+ahmedhienz@users.noreply.github.com>
2018-11-01 23:10:19 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace adv_e_commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
