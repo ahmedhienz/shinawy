f67455a075fa3ef0ce846fd8110b333e5fdc01e4
ahmedhienz <44682086+ahmedhienz@users.noreply.github.com>
2018-11-01 23:02:51 +0200

    Add files via upload

---
+++ Controllers/hello.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvcmovie.controllers
+{
+
+    public class hello : Controller
+    {
+       
+   
+    public string Index ()
+    
+    {
+        return "This is my default action.. ";
+    }
+
+    
+    public string welcome()
+    {
+        return "This is the Welcome action method";
+    }
+}
+}
