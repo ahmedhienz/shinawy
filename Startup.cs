7df21e029ed12e42b0409849b3c0e91505891811
ahmedhienz <44682086+ahmedhienz@users.noreply.github.com>
2018-11-01 23:17:34 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,8 +37,10 @@ namespace adv_e_commerce
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-            }
-
+             }
+             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
